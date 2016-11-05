9d2eb8bb1dae72e34a4a0b62439cc48b79540aef
Thang Tong <tongthang0104@gmail.com>
2016-11-04 22:30:22 -0700

    (feat) simple file structure
    Simple server setup

---
+++ server/middleware.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const bodyParser  = require('body-parser');
+const session = require('express-session');
+const path = require('path');
+
+module.exports = function (app, express) {
+  const routes = express.Router();
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(express.static(path.join(__dirname, 'client')));
+};
