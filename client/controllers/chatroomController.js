7696ed4c474c81f29b574599d2e52b473683811b
Thang Tong <tongthang0104@gmail.com>
2016-11-08 02:21:54 -0800

    (fix) Clean up

--- client/controllers/chatroomController.js
+++ client/controllers/chatroomController.js
@@ -19,7 +19,6 @@ function chatroomCtrl($scope, services, $interval) {
       $scope.logined = true;
       $scope.user = data;
 
-      console.log('user is registering:', data);
       $scope.notification = {
         message: data.user.nickname,
         type: 'notification'
@@ -57,7 +56,6 @@ function chatroomCtrl($scope, services, $interval) {
     services.fetchAllMessages().then(function(response) {
       $scope.messages = response.data;
       $scope.messages.unshift($scope.notification);
-      console.log('all messages', response.data);
     }).catch(function(error) {
       console.error(error);
     });
